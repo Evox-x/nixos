c63d6608127bf1b50df76dfaedde3435a798be27
Evox-x <evox@x.ch>
2026-02-21 02:32:26 +0100

    enable niri

--- configuration.nix
+++ configuration.nix
@@ -72,9 +72,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Enable the GNOME and NIRI Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  programs.niri.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   home.username = "evoxx";
   home.homeDirectory = "/home/evoxx";
 
-  home.stateVersion = "23.11"; # or your NixOS version
+  home.stateVersion = "25.11"; 
 
   home.packages = with pkgs; [
     niri
