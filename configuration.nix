7d8555befeeba5741db4376fdcef01c76bb64659
Evox-x <evox@x.ch>
2026-02-24 15:02:28 +0100

    fix: niri jetbrains rendering

--- configuration.nix
+++ configuration.nix
@@ -180,10 +180,10 @@
       ];
     };
   };
-  #niri
+  #niri functions
   programs.xwayland.enable = true;
   services.dbus.enable = true;
   environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "x11";
   };
 }
